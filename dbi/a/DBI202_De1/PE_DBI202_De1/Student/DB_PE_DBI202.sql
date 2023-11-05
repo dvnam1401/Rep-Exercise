e4c22771d3b5fbcfb90bb8d3e0d4fd2a0032360c
Văn Nam <vannamdang2003@gmail.com>
2023-11-05 09:24:18 +0700

    da

--- dbi/a/DBI202_De1/PE_DBI202_De1/Student/DB_PE_DBI202.sql
+++ dbi/a/DBI202_De1/PE_DBI202_De1/Student/DB_PE_DBI202.sql
@@ -81,3 +81,70 @@ BEGIN
 Insert into tblCustomer values(@CustID,@CustName,@DOB,@Tel)
 
 END;
+
+CREATE TABLE tblRoomReservation
+(
+    ReservationID INT PRIMARY KEY IDENTITY(1,1),
+    TypeID VARCHAR(3) FOREIGN KEY REFERENCES tblRoomType(TypeID),
+    OrderID VARCHAR(10) FOREIGN KEY REFERENCES tblHotelOrder(OrderID),
+    ServiceID VARCHAR(10),
+    ServiceName NVARCHAR(50),
+    ServiceCharge DECIMAL(10,2),
+    UsageDateTime DATETIME,
+    NumberOfTimesUsed INT
+);
+
+INSERT INTO tblRoomReservation (TypeID, OrderID, ServiceID, ServiceName, ServiceCharge, UsageDateTime, NumberOfTimesUsed)
+VALUES 
+('STD', '001', 'S001', N'Room Cleaning', 100.00, '2023-04-26 10:00:00', 1),
+('DLX', '002', 'S002', N'Laundry', 150.00, '2023-04-19 15:30:00', 2),
+('SUT', '003', 'S003', N'In-Room Dining', 250.00, '2023-05-11 20:45:00', 1);
+ 
+CREATE PROCEDURE AddNewCustomer
+@CustID VARCHAR(10),
+@CustName NVARCHAR(40),
+@DOB DATE,
+@Tel CHAR(10)
+AS
+BEGIN
+    BEGIN TRY
+        INSERT INTO tblCustomer (CustID, CustName, DOB, Tel)
+        VALUES (@CustID, @CustName, @DOB, @Tel)
+    END TRY
+    BEGIN CATCH
+        PRINT 'Error encountered while adding new customer. Please check the data and try again.'
+    END CATCH
+END;
+
+CREATE TRIGGER tr_AssignRoomToOrder
+ON tblRoomOrder
+AFTER INSERT
+AS
+BEGIN
+    DECLARE @RoomID VARCHAR(4)
+    SELECT @RoomID = RoomID FROM INSERTED
+
+    IF EXISTS (SELECT 1 FROM tblRoom WHERE RoomID = @RoomID AND Rstatus = 'NA')
+    BEGIN
+        ROLLBACK TRANSACTION
+        PRINT 'Cannot assign this room as it is not available.'
+    END
+    ELSE
+    BEGIN
+        UPDATE tblRoom
+        SET Rstatus = 'NA'
+        WHERE RoomID = @RoomID
+    END
+END;
+
+CREATE VIEW v_RepeatedCustomers AS
+SELECT 
+    c.CustID,
+    c.CustName,
+    c.DOB,
+    c.Tel,
+    COUNT(o.OrderID) AS NumberOfReservations
+FROM tblCustomer c
+JOIN tblHotelOrder o ON c.CustID = o.CustID
+GROUP BY c.CustID, c.CustName, c.DOB, c.Tel
+HAVING COUNT(o.OrderID) > 1;
